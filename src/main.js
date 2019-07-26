344838313304a44c55dea473115e5517f80cea0f
‘knry0329’ <bxnxsxenathxy@gmail.com>
2019-07-26 20:37:39 +0900

    first commit .

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebaseConfig from './config/firebase'
+
+import firebase from 'firebase' // 追記
+
+import ElementUI from 'element-ui' // 追記
+import locale from 'element-ui/lib/locale/lang/ja' // 追記
+import 'element-ui/lib/theme-chalk/index.css' // 追記
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, { locale })
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
