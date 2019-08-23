c5ae02fbe53463d443dd9637c0f4f4eedb89936f
knry0329 <bxnxsxenathxy@gmail.com>
2019-08-23 20:55:43 +0900

    add Docker file .

--- src/router.js
+++ src/router.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
@@ -5,7 +6,7 @@ import firebase from 'firebase'
 
 Vue.use(Router)
 
-let router = new Router({
+  let router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -15,8 +16,7 @@ let router = new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      // eslint-disable-next-line no-unused-vars
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue') 
     },
     {
       path: '/searchbook',
@@ -75,6 +75,7 @@ let router = new Router({
 
   ]
 })
+
 
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    configureWebpack: {
-      devtool: 'source-map'
-    },
-    devServer: {
-        host: "localhost",
-        port: "8081"
-      }
-  }
+  configureWebpack: {
+    devtool: 'source-map'
+  },
+  devServer: {
+    host: 'localhost',
+    port: '8081'
+  },
+  publicPath: './'
+}
